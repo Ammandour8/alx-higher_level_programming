687733fd8790aae46ea676cb77fc7436e2f877de
Ammandour8 <amrmandour51@gmail.com>
2024-04-25 16:23:09 -0400

    Hot Fix

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -s "$1" -d "@$2" -X POST -H "Content-Type: application/json"
+# takes in a URL, sends a POST request to the passed URL, and displays the body of the response
+curl -sX POST $1 -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -L
