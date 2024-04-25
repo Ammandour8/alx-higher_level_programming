55925a4eede015ff2346b81ddeb7ad03c937b265
Ammandour8 <amrmandour51@gmail.com>
2024-04-25 15:48:14 -0400

    update

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a request to a URL passed as an argument, and displays only the status code of the response. no pipe
-awk 'NR==1{printf "%s", $2}' test7 $(curl -sI "$1" -o test7)
+# Displays the body of the response of a curl POST request with json
+curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
